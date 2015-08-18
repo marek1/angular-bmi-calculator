7dd4ba700f0c2645a8f7358fa9798c6d3733cebd
marek1 <cyclingfellas@gmail.com>
2015-08-18 18:30:29 +0200

    Version 0.0.3 : Template inline with gulp task (Bower data updated)

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
   jasmine = require('gulp-jasmine'),
   uglify = require('gulp-uglify'),
-  rename = require('gulp-rename');
-  Server = require('karma').Server;
+  rename = require('gulp-rename'),
+  Server = require('karma').Server,
+  htmlmin = require('gulp-htmlmin'),
+  replace = require('gulp-replace'),
+  fs = require('fs');
 
 gulp.task('jshint', function() {
   return gulp.src('*.js')
@@ -12,14 +15,6 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('default', { verbose: true }));
 });
 
-gulp.task('compress', function() {
-  return gulp.src('bmi-calculator.js')
-    .pipe(uglify())
-    .pipe(rename({
-      extname: '.min.js'
-    }))
-    .pipe(gulp.dest('dist'));
-});
 
 gulp.task('connect', function() {
   connect.server({
@@ -39,4 +34,33 @@ gulp.task('watch', function() {
   gulp.watch("*.js", ["jshint", "karma"]);
 });
 
+gulp.task('htmlmin', function() {
+  return gulp.src('bmi-calculator.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('tmp'))
+});
+
+gulp.task('replace_html', ['htmlmin'], function(){
+  return gulp.src(['tmp/bmi-calculator.html'])
+    .pipe(replace(/'/g, "\\'"))
+    .pipe(gulp.dest('tmp/'));
+});
+
+gulp.task('replace_js', ['htmlmin', 'replace_html'], function(){
+  return gulp.src(['bmi-calculator.js'])
+    .pipe(replace('templateUrl', 'template'))
+    .pipe(replace('bmi-calculator.html', fs.readFileSync('tmp/bmi-calculator.html')))
+    .pipe(gulp.dest('tmp/'));
+});
+
+gulp.task('compress', ['htmlmin', 'replace_html', 'replace_js'],  function() {
+  return gulp.src('tmp/bmi-calculator.js')
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('default', ['connect', 'watch']);
+gulp.task('build', ['htmlmin', 'replace_html', 'replace_js', 'compress']);
